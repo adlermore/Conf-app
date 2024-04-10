82043561d6f18fef8b77526ee6019f89af35b82b
adlermore <armorbita@gmail.com>
2024-04-10 19:06:48 +0400

    first commit

---
+++ app/(root)/meeting/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React  from 'react'
+
+const Meeting = ({params}: {params : {id: string}}) => {
+  return (
+    <div className='relative'>
+      Meeting Room: #{params.id}
+    </div>
+  )
+}
+
+export default Meeting
---
+++ components/MeetingTypeList.tsx
@@ -0,0 +1,71 @@
+'use client';
+
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+
+import HomeCard from './HomeCard';
+// import MeetingModal from './MeetingModal';
+// import { Call, useStreamVideoClient } from '@stream-io/video-react-sdk';
+import { useUser } from '@clerk/nextjs';
+import MeetingModal from './MeetingModal';
+// import Loader from './Loader';
+// import { Textarea } from './ui/textarea';
+// import ReactDatePicker from 'react-datepicker';
+// import { useToast } from './ui/use-toast';
+// import { Input } from './ui/input';
+
+const initialValues = {
+  dateTime: new Date(),
+  description: '',
+  link: '',
+};
+
+const MeetingTypeList = () => {
+  const router = useRouter();
+  const [meetingState, setMeetingState] = useState<
+    'isScheduleMeeting' | 'isJoiningMeeting' | 'isInstantMeeting' | undefined
+  >(undefined);
+  const [values, setValues] = useState(initialValues);
+
+
+  return (
+    <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
+      <HomeCard
+        img="/icons/add-meeting.svg"
+        title="New Meeting"
+        description="Start an instant meeting"
+        handleClick={() => setMeetingState('isInstantMeeting')}
+      />
+      <HomeCard
+        img="/icons/join-meeting.svg"
+        title="Join Meeting"
+        description="via invitation link"
+        className="bg-blue-1"
+        handleClick={() => setMeetingState('isJoiningMeeting')}
+      />
+      <HomeCard
+        img="/icons/schedule.svg"
+        title="Schedule Meeting"
+        description="Plan your meeting"
+        className="bg-purple-1"
+        handleClick={() => setMeetingState('isScheduleMeeting')}
+      />
+      <HomeCard
+        img="/icons/recordings.svg"
+        title="View Recordings"
+        description="Meeting Recordings"
+        className="bg-yellow-1"
+        handleClick={() => router.push('/recordings')}
+      />
+      <MeetingModal
+        isOpen={meetingState === 'isInstantMeeting'}
+        onClose={() => setMeetingState(undefined)}
+        title="Start an Instant Meeting"
+        className="text-center"
+        buttonText="Start Meeting"
+      />
+    </section>
+  );
+};
+
+export default MeetingTypeList;
